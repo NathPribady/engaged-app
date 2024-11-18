1b01fac76365840d7a5bb3c5529f5409a2ea2a85
Nathanael Pribady <dearnathanael@gmail.com>
2024-11-17 20:30:10 -0500

    delete unused code

--- app/interactive-lesson/view/page.tsx
+++ app/interactive-lesson/view/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
